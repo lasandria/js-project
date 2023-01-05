fd6f4df2e09acf2c89aaa27b8c45ff1855577d88
Sasha <alzayceva@mail.ru>
2023-01-05 16:17:54 +0300

    first JS commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// "use strict"; // работаем в современном режиме
+
+// let number = 1; // можно менять переменную
+// const borderLeft = 5; //нельзя менять переменную. но прямых констант в джс нет
+// let numberOne  = 4;
+
+// // alert('ПРивет')
+
+// const result = confirm('Как жизнь?');
+// console.log(result);
+// const answer = +prompt('Вам есть 18?', '18 мне уже');
+// console.log(typeof(answer));
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше мя', '');
+// answers[1] = prompt('Как ваше мя', '');
+// answers[2] = prompt('Как ваше мя', '');
+
+
+// console.log(0);
+
+// const user = "ivan";
+
+// alert(`hello, ${user}`);
+
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log (incr, decr);
+
+console.log(5%2, 56%2, 8%2);
