27f4912433260454c8628f9862124c72d89b42b5
Flexa522 <lasandria@mail.ru>
2023-01-10 13:35:21 +0300

    js practic 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // "use strict"; // работаем в современном режиме
 
 // let number = 1; // можно менять переменную
-// const borderLeft = 5; //нельзя менять переменную. но прямых констант в джс нет
+// const borderLeft = 5; 
 // let numberOne  = 4;
 
 // // alert('ПРивет')
@@ -35,6 +35,30 @@
 
 
 
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
+
+
+
+
+// personalMovieDB.movies[a] = b;
+// console.log(personalMovieDB);
+
+// Циклы
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 
@@ -46,14 +70,22 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
+ 
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
+    personalMovieDB.movies[a] = b;
 
-personalMovieDB.movies[a] = b;
-console.log(personalMovieDB)
+    if (a != null && b!=null&& a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
+console.log(personalMovieDB);   
--- js/teory.js
+++ js/teory.js
@@ -30,4 +30,58 @@
 // decr--;
 // console.log (incr, decr);
 
-console.log(5%2, 4746%2, 8%2);
+// console.log(5%2, 4746%2, 8%2);
+
+
+// if (4 == 9) {
+//     console.log(Ok);
+// } else {
+//     console.log('Error')
+// }
+
+// const num = +prompt('Цифра', '')
+
+// if (num < 49) {
+//     alert('Error');
+// } else if (num > 50) {
+//     alert('много');
+// }
+//   else {
+//     alert('Вы -гений!');
+// }
+
+//Циклы
+
+// let num = 50;
+
+// while( num<=60 ) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num<55);
+
+// let num = 50;
+// for (let i = 1; i<3; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// for (let i = 1; i<10; i++) {
+//     if (i===6) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+for (let i = 1; i<10; i++) {
+    if (i===6) {
+        continue;
+    }
+    console.log(i);
+}
